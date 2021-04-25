bf89a7ea0c1f47f0ef17869c192a72bf1c7b78c7
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2021-04-25 13:54:15 +0200

    Modified SignalGenerator method.

--- laso.driver/Utility/SignalGenerator.cs
+++ laso.driver/Utility/SignalGenerator.cs
@@ -11,7 +11,7 @@ namespace laso.driver.Utility
         const double samplingFrequency = 44.1e3;
         const int sampleNumber = 4096;
 
-        public static double[] SineWaveRawSignal(
+        public static double[] SineWaveRaw(
             double signalFrequency = 1e3,
             double samplingFrequency = samplingFrequency,
             double amplitude = 1,
@@ -28,11 +28,18 @@ namespace laso.driver.Utility
             return signal;
         }
 
+        /// <summary>
+        /// Returns an array of frequencies as detemrined by the sampling rate and number of samples.
+        /// </summary>
+        /// <param name="samplingFrequency">Sampling frequency.</param>
+        /// <param name="sampleNumber">Number of samples.</param>
+        /// <param name="halfRange">Whether it should return only half of the frequency range.</param>
+        /// <returns></returns>
         public static double[] Frequencies(
             double samplingFrequency = samplingFrequency,
-            int sampleNumber = sampleNumber, bool halfSpectrum = true)
+            int sampleNumber = sampleNumber, bool halfRange = true)
         {
-            int frequencyBins = halfSpectrum ? sampleNumber / 2 : sampleNumber;
+            int frequencyBins = halfRange ? sampleNumber / 2 : sampleNumber;
             double[] frequencies = new double[frequencyBins];
 
             for(int i = 0; i< frequencyBins; i++)
